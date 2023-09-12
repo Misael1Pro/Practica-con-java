946b5da7c92c35a716f595640ffb2ee13211c639
Misael <betancourthmisael2001@gmail.com>
2023-09-11 20:32:28 -0600

    Agregando comentario a las lineas de codigo

--- intro.js
+++ intro.js
@@ -1,9 +1,9 @@
-let miRefran;
-miRefran= "Camarón que se duerme, se lo lleva la corriente"
-console.log(miRefran)
-let longitudRefran;
-longitudRefran=miRefran.length
-console.log(longitudRefran)
-novenaletra= console.log(miRefran[8])
-ultimaletra=console.log(miRefran[miRefran.length-1])
-Mayuscula= console.log(miRefran.toUpperCase())
+let miRefran;  //declaracion de variable
+miRefran= "Camarón que se duerme, se lo lleva la corriente" //Asignacion de texto a la variable
+console.log(miRefran) //Muestra en pantalla el refran
+let longitudRefran; //declaracion de variable para vver el tamaño del refran
+longitudRefran=miRefran.length 
+console.log(longitudRefran) //Imprimiendo longitud de regran 
+novenaletra= console.log(miRefran[8]) //Mostrando la novena letra del refran 
+ultimaletra=console.log(miRefran[miRefran.length-1]) //Mostrando la ultima letra del refran 
+Mayuscula= console.log(miRefran.toUpperCase()) //Convirtiendo a mayuscula todo el refran
