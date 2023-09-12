9eafa4656abd995070764db7d0c08a159f5ba54b
Misael <betancourthmisael2001@gmail.com>
2023-09-11 20:26:25 -0600

    Práctica1 java

---
+++ intro.js
@@ -0,0 +1,9 @@
+let miRefran;
+miRefran= "Camarón que se duerme, se lo lleva la corriente"
+console.log(miRefran)
+let longitudRefran;
+longitudRefran=miRefran.length
+console.log(longitudRefran)
+novenaletra= console.log(miRefran[8])
+ultimaletra=console.log(miRefran[miRefran.length-1])
+Mayuscula= console.log(miRefran.toUpperCase())
